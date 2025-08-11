3bb23d97198011b9dad607c44da9501f2a5e7f88
Kenn Vun Gan <kennvun@hotmail.com>
2025-08-11 23:25:38 +0900

    chore(test): update test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Discover More button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const discoverMoreButton = screen.getByRole('button', { name: /discover more/i });
+  expect(discoverMoreButton).toBeInTheDocument();
 });
